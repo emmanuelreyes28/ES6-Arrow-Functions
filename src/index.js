125e46b488c6cac5c72222628519200757efa82f
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-10-27 16:43:52 -0700

    ES6 Arrow Functions

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
+import App from "./components/App";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App/>);
+//ReactDOM.render(<App />, document.getElementById("root"));
+
+let numbers = [3, 56, 2, 48, 5];
+
+// const newNumbers = numbers.map(x => x * x);
+
+// console.log(newNumbers);
+
+////Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map(x => x * 2);
+// console.log(newNumbers);
+
+//////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(num => num < 10);
+// console.log(newNumbers);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+// const newNumber = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+// console.log(newNumber);
+
+////Find - find the first item that matches from an array.
+// const newNumber = numbers.find(num => num > 10);
+// console.log(newNumber);
+
+////FindIndex - find the index of the first item that matches.
+const newNumber = numbers.findIndex(num => num > 10);
+console.log(newNumber);
